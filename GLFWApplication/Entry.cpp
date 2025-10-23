44ab221fc2037fffe13535797871f5137c1bd3d4
Devious3D <wrightdevon505@gmail.com>
2025-10-23 04:54:03 -0500

    SDL Installed <Compiles>

--- GLFWApplication/Entry.cpp
+++ GLFWApplication/Entry.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
-
+#include <SDL3/SDL.h>
 #include "Entry.h"
 
 
-int main() {
-	using namespace std;
 
-	cout << "Hello World";
+int main() {
+
+	{
+		using namespace std;
+		cout << "Hello World" << endl;
+		cout << "Hello World Again!" << endl;
+	}
+
+	
+
+
 
 	return 0;
 }
+
